d275f931ee5edef615f8ecc86c6a4f6ca397e871
Makara <makaragit.sam@2023.com>
2024-06-20 20:02:17 +0700

    bound_controls

---
+++ Chapter 19/CutomerMaintenance/CutomerMaintenance/frmCustomer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CutomerMaintenance
+{
+    public partial class frmCustomer : Form
+    {
+        public frmCustomer()
+        {
+            InitializeComponent();
+        }
+
+        private void customersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.customersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.mMABooksDataSet);
+
+        }
+
+        private void frmCustomer_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'mMABooksDataSet.Customers' table. You can move, or remove it, as needed.
+            this.customersTableAdapter.Fill(this.mMABooksDataSet.Customers);
+            // TODO: This line of code loads data into the 'mMABooksDataSet.Customers' table. You can move, or remove it, as needed.
+            this.customersTableAdapter.Fill(this.mMABooksDataSet.Customers);
+
+        }
+
+        private void fillByToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void customersBindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.customersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.mMABooksDataSet);
+
+        }
+
+        private void fillBy1ToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.customersTableAdapter.FillBy1(this.mMABooksDataSet.Customers);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void fillByCustomerIDToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // this.customersTableAdapter.FillByCustomerID(this.mMABooksDataSet.Customers, ((int)(System.Convert.ChangeType(customerIDToolStripTextBox.Text, typeof(int)))));
+                int customerID = Convert.ToInt32(
+                    customerIDToolStripTextBox.Text
+                    );
+
+                this.customersTableAdapter.FillByCustomerID(
+                    this.mMABooksDataSet.Customers,customerID
+                    );
+            }
+            catch (System.Exception ex)
+            {
+                // System.Windows.Forms.MessageBox.Show(ex.Message);
+                MessageBox.Show(ex.Message,
+                    ex.GetType().ToString()
+                    );
+            }
+
+        }
+    }
+}
